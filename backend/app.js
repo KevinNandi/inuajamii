e21e8ef942b4960430a37ab8cc01e3f865af6d29
KevinNandi <362kevin@gmail.com>
2023-10-09 15:50:22 +0300

    finishing up backend
    backend finishing

--- backend/app.js
+++ backend/app.js
@@ -3,14 +3,11 @@ const ErrorHandler = require("./utils/ErrorHandler");
 const app = express();
 const cookieParser = require("cookie-parser");
 const bodyParser = require("body-parser");
-const fileUpload = require("express-fileupload");
-
+ 
 app.use(express.json());
 app.use(cookieParser());
+app.use("/", express.static("uploads"));
 app.use(bodyParser.urlencoded({extended:true} ));
-app.use(fileUpload({useTempFiles: true}));
-
-
 
 // config
 if (process.env.NODE_ENV !== "PRODUCTION") {
@@ -19,7 +16,13 @@ if (process.env.NODE_ENV !== "PRODUCTION") {
     });
   }
 
+  // import routes
+  const user = require("./controller/user");
+
+  app.use("/api/v2/user", user);
+
 
 // it's for ErrorHandling
 app.use(ErrorHandler);
+
 module.exports = app;
